07ba6927fdf1d805720274f7ecc6a4a4940d2608
Zhihao Zhang <zhihao.zhang1@unsw.edu.au>
2020-01-17 17:34:19 +1100

    add incrementing data pointer

--- include/interpreter.h
+++ include/interpreter.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include <array>
+namespace bf
+{
+    auto constexpr data_stack_size = 30000;
+    using data_stack_type = std::array<char,data_stack_size>;
+
+    inline auto increment_data_pointer(data_stack_type::iterator data_pointer) {
+        return ++data_pointer;
+    }
+}
--- test/interpreter.cpp
+++ test/interpreter.cpp
@@ -1,3 +1,41 @@
-#include "gtest/gtest.h"
 #include "interpreter.h"
+
+#include "gtest/gtest.h"
+#include <array>
+
+namespace
+{
+
+class interpreter_test : public ::testing::TestWithParam<std::size_t>
+{
+public:
+    void SetUp() override
+    {
+        index_ = GetParam();
+        data_stack_[index_ + 1] = indicator_value;
+    }
+    void TearDown() override{}
+
+protected:
+    static char constexpr indicator_value = 'H';
+    bf::data_stack_type data_stack_ {};
+    std::size_t index_ {};
+};
+    
+TEST_P(interpreter_test, increment_data_pointer)
+{
+    auto const data_pointer =
+        bf::increment_data_pointer(std::begin(data_stack_)+index_);
+    EXPECT_EQ(indicator_value,
+        *bf::increment_data_pointer(std::begin(data_stack_) + index_));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    interpreter_test_suit, interpreter_test,
+    // last valid data stack pointer is (data_stack_size-1)
+    ::testing::Values(0, 1, 100, 1000, 10000, bf::data_stack_size-2));
+} // namespace
+/*
+ * \brief Increment 
+ */
 
